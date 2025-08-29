2bc18710b11883927d11a7f92149f173a765478a
nick <69575053+nickoncrack@users.noreply.github.com>
2025-08-29 19:49:18 +0300

    Add PS/2 macros

--- arch/i386/include/asm/io.h
+++ arch/i386/include/asm/io.h
@@ -20,8 +20,41 @@
 #define PS2_WRITE_PORT2 0xD4
 #define PS2_DIS_PORT2   0xA7
 #define PS2_ENB_PORT2   0xA8
+#define PS2_TEST_PORT2  0xA9
+#define PS2_TEST        0xAA
+#define PS2_TEST_PORT1  0xAB
 #define PS2_DIS_PORT1   0xAD
 #define PS2_ENB_PORT1   0xAE
+#define PS2_IDENTIFY    0xF2
+#define PS2_ENB_SCAN    0xF4
+#define PS2_DIS_SCAN    0xF5
+#define PS2_ACK         0xFA
+
+#define PS2_ID_STD_MOUSE    0x00
+#define PS2_ID_SCRL_MOUSE   0x03
+#define PS2_ID_5BTN_MOUSE   0x04
+
+#define PS2_TEST_CTRL_PASS  0x55
+#define PS2_TEST_CTRL_FAIL  0xFC
+#define PS2_TEST_PORT_PASS  0x00
+#define PS2_TEST_CLK_LO     0x01
+#define PS2_TEST_CLK_HI     0x02
+#define PS2_TEST_DAT_LO     0x03
+#define PS2_TEST_DAT_HI     0x04
+
+#define PS2_MOUSE_SET_RATE  0xF3
+#define PS2_MOUSE_DEFAULT   0xF6
+#define PS2_MOUSE_RESET     0xFF
+#define PS2_MOUSE_BIT_LMB   0x01
+#define PS2_MOUSE_BIT_RMB   0x02
+#define PS2_MOUSE_BIT_MMB   0x04
+#define PS2_MOUSE_BIT_A1    0x08
+#define PS2_MOUSE_BIT_BTN4  0x10
+#define PS2_MOUSE_BIT_BTN5  0x20
+#define PS2_MOUSE_BIT_XS    0x10
+#define PS2_MOUSE_BIT_YS    0x20
+#define PS2_MOUSE_BIT_XO    0x40
+#define PS2_MOUSE_BIT_YO    0x80
 
 #define PIT_SET  0x36
 #define PIT_DAT0 0x40
